9fdce2b9fac0ec527f745a14020f3cafab032073
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-10-29 18:58:23 +0530

    used toggle component in metric unit and standard unit component

--- src/pages/MetricUnit.jsx
+++ src/pages/MetricUnit.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import swal from "@sweetalert/with-react";
 import MetricUnitForm from "../components/MetricUnitForm";
 import getBmi from "../utils/getBmi";
 import getCategory from "../utils/getCategory";
+import Toggle from "../components/Toggle";
 
 const MetricUnit = () => {
   const handleFormSubmit = (height, weight) => {
@@ -18,24 +18,7 @@ const MetricUnit = () => {
   return (
     <div className="metricunit">
       <div className="metricunit__card">
-        <div className="metricunit__toggle">
-          <NavLink
-            to="/standard-unit"
-            tabIndex={0}
-            activeClassName="metricunit__toggle--active"
-            className="metricunit__toggle--su metricunit__toggle__menu"
-          >
-            STANDARD UNIT
-          </NavLink>
-          <NavLink
-            to="/metric-unit"
-            tabIndex={0}
-            activeClassName="metricunit__toggle--active"
-            className="metricunit__toggle--mu metricunit__toggle__menu"
-          >
-            METRIC UNIT
-          </NavLink>
-        </div>
+        <Toggle />
         <MetricUnitForm onMetricUnitFormSubmit={handleFormSubmit} />
       </div>
     </div>
--- src/pages/StandardUnit.jsx
+++ src/pages/StandardUnit.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import StandardUnitForm from "../components/StandardUnitForm";
 import getBmi from "../utils/getBmi";
 import getCategory from "../utils/getCategory";
 import swal from "@sweetalert/with-react";
+import Toggle from "../components/Toggle";
 
 const StandardUnit = () => {
   const handleFormSubmit = (height, heightInInches, weight) => {
@@ -18,24 +18,7 @@ const StandardUnit = () => {
   return (
     <div className="standardunit">
       <div className="standardunit__card">
-        <div className="standardunit__toggle">
-          <NavLink
-            to="/standard-unit"
-            tabIndex={0}
-            activeClassName="metricunit__toggle--active"
-            className="metricunit__toggle--su metricunit__toggle__menu"
-          >
-            STANDARD UNIT
-          </NavLink>
-          <NavLink
-            to="/metric-unit"
-            tabIndex={0}
-            activeClassName="metricunit__toggle--active"
-            className="metricunit__toggle--mu metricunit__toggle__menu"
-          >
-            METRIC UNIT
-          </NavLink>
-        </div>
+        <Toggle />
         <StandardUnitForm onStandardUnitFormSubmit={handleFormSubmit} />
       </div>
     </div>
