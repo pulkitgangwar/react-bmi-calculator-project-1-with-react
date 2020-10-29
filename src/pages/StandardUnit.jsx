c97541bb03f07f59786487908dfcd2f13129e42d
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-10-29 17:43:17 +0530

    form is working now

--- src/pages/StandardUnit.jsx
+++ src/pages/StandardUnit.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import StandardUnitForm from "../components/StandardUnitForm";
+import getBmi from "../utils/getBmi";
+import getCategory from "../utils/getCategory";
+import swal from "@sweetalert/with-react";
 
 const StandardUnit = () => {
+  const handleFormSubmit = (height, heightInInches, weight) => {
+    const bmi = getBmi({ height, heightInInches, weight }, "imperialUnit");
+    const { category } = getCategory(bmi);
+    swal({
+      title: "RESULT",
+      text: `${bmi} kg/m(2)  -  ${category}`,
+    });
+  };
+
   return (
     <div className="standardunit">
       <div className="standardunit__card">
@@ -24,7 +36,7 @@ const StandardUnit = () => {
             METRIC UNIT
           </NavLink>
         </div>
-        <StandardUnitForm onStandardUnitFormSubmit={() => {}} />
+        <StandardUnitForm onStandardUnitFormSubmit={handleFormSubmit} />
       </div>
     </div>
   );
